ba1c425b03f115f015f61dbc5edbdac61ed7fffd
vjvl95 <vjvl95@naver.com>
2023-03-19 18:42:19 +0900

    design(*): reset 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import './styles/reset.css';
 import Router from './routes';
+
 function App() {
   return (
     <BrowserRouter>
