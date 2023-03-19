6a4c68e922a3f7af60e2ffb2eea5b9fff56a0e81
vjvl95 <vjvl95@naver.com>
2023-03-19 23:41:47 +0900

    feat(main): main에 qna container 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,24 +1,31 @@
 import Button from '../components/common/Button';
 import InputBox from '../components/common/InputBox';
-
+import InputTilte from '../components/common/InputTitle';
+import MainQnaCard from '../components/common/MainQnaCard';
+import '../styles/main.scss';
 export default function Main() {
   return (
     <>
       <Button margin='10px' isOutline>
         텍스트
       </Button>
-
       <Button margin='10px'>텍스트</Button>
-
       <Button margin='10px' width='sm' isOutline>
         텍스트
       </Button>
-
       <Button margin='10px' width='sm'>
         텍스트
       </Button>
-
       <InputBox />
+      <div className='main-qna-container'>
+        <MainQnaCard />
+        <MainQnaCard />
+        <MainQnaCard />
+        <MainQnaCard />
+        <MainQnaCard />
+      </div>
+      <InputTilte isRequire={true} width='200px' />
+      <InputTilte width='200px' />
     </>
   );
 }
