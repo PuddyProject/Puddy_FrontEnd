4950c089d097a24fcda1d54c5ab54d120664b249
vjvl95 <vjvl95@naver.com>
2023-03-19 16:53:00 +0900

    feat(*): 공통 컴포넌트 Button 기능 완성(#15)

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../../styles/button.scss';
+import classnames from 'classnames';
+
+interface ButtonProps {
+  onClick?: () => void;
+  children: React.ReactNode;
+  width?: string;
+  isOutline?: boolean;
+  margin?: string;
+  padding?: string;
+  customSize?: string;
+}
+
+export default function Button({
+  children,
+  customSize,
+  isOutline,
+  onClick,
+  width,
+  margin,
+  padding,
+}: ButtonProps) {
+  const buttonStyle = {
+    margin: margin,
+    padding: padding,
+    width: customSize,
+  };
+
+  return (
+    <button className={classnames('button', width, { isOutline })} style={buttonStyle}>
+      {children}
+    </button>
+  );
+}
