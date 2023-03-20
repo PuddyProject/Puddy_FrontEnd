425a2449659dff60f005464a842d3d970afac969
ReturnReason <reasonz@daum.net>
2023-03-20 17:10:06 +0900

    chore: ts 확장자로 변경

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Main } from './Main/';
+export { default as Login } from './Login/';
+export { default as Qna } from './QnA';
+export { default as Community } from './Community';
