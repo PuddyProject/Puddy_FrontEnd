42c0e38e8fd15c2bf0a3a0cf4a761712eb8997b0
ReturnReason <reasonz@daum.net>
2023-03-21 17:21:28 +0900

    chore: 필요없는 코드 제거

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
 export { default as Button } from './common/Button';
-export { default as BottomAppBar } from './layouts/bottom/AppBar';
-export { default as HeaderAppBar } from './layouts/header/AppBar';
 export { default as InputBox } from './common/InputBox';
 export { default as QnACard } from './common/MainQnaCard';
 export { default as Message } from './common/Message';
