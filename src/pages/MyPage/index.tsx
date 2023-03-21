be738a4505bd0b608d697bdbc865727226de81f2
ReturnReason <reasonz@daum.net>
2023-03-22 06:12:44 +0900

    feat(MyPage): 마이페이지 레이아웃, 스타일 적용 (#4)

---
+++ src/pages/MyPage/index.tsx
@@ -0,0 +1,59 @@
+import { FiSettings as SettingIcon } from 'react-icons/fi';
+import { Button } from 'components';
+import { Tier, myPageList, MypageListItem } from 'constants/myPageList';
+
+const TEMP_IMAGE_URL =
+  'https://blog.kakaocdn.net/dn/GHYFr/btrsSwcSDQV/UQZxkayGyAXrPACyf0MaV1/img.jpg';
+
+const ProfileSettingButton = () => {
+  return (
+    <button className='circle-button'>
+      <SettingIcon />
+    </button>
+  );
+};
+
+const MenuItems = (list: Array<MypageListItem>) => {
+  return list.map((item, i) => {
+    const isTitle = item.tier === Tier.TITLE;
+
+    return (
+      <li key={`${item.title} ${i}`} role='button'>
+        {isTitle ? (
+          <h2 className='menu-title'>{item.title}</h2>
+        ) : (
+          <>
+            <p className='menu-content' tabIndex={0} role='button'>
+              {item.title}
+            </p>
+            {/* // TODO: '?' 아이콘 추가 후 클릭 시 모달 띄우기
+            {item.icon && <button className='qu estion-mark-icon'>전문가가 무엇인가요?</button>} */}
+          </>
+        )}
+      </li>
+    );
+  });
+};
+
+export default function MyPage() {
+  return (
+    <>
+      <div className='container'>
+        <section className='profile'>
+          <img className='profile-image' src={TEMP_IMAGE_URL} alt='프로필 이미지' />
+          <div className='setting'>
+            <ProfileSettingButton />
+          </div>
+          <h3 className='nickname'>닉네임</h3>
+          <Button outline width='200px' height='35px'>
+            내 펫 정보
+          </Button>
+        </section>
+        <hr className='dividing-line' />
+        <section className='setting-menus'>
+          <ul>{MenuItems(myPageList)}</ul>
+        </section>
+      </div>
+    </>
+  );
+}
