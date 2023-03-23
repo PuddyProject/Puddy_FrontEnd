7b9c2d24b868b1daf472a11074820fd2fff6e7a5
vjvl95 <vjvl95@naver.com>
2023-03-24 02:28:50 +0900

    feat(QnaContainer): 메인화면에 쓰일 Q&A 컴포넌트로 분리

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,24 +1,21 @@
 import Button from 'components/common/Button';
-import InputBox from 'components/common/InputBox';
-import MainQnaCard from 'components/common/MainQnaCard';
+
+import QnaContainer from 'components/main/QnaContainer';
 
 export default function Main() {
   return (
     <div className='main'>
-      <Button margin='10px' width='160px'>
-        Q&A 질문하기
-      </Button>
-      <Button margin='10px' outline width='160px'>
-        내 펫 등록하기
-      </Button>
-
-      <InputBox />
-      <div className='main-qna-container'>
-        {Array(5)
-          .fill(0)
-          .map((_, i) => {
-            return <MainQnaCard key={i} />;
-          })}
+      <div className='button-container'>
+        <Button margin='10px' width='160px'>
+          Q&A 질문하기
+        </Button>
+        <Button margin='10px' outline width='160px'>
+          내 펫 등록하기
+        </Button>
+      </div>
+      <div>
+        <QnaContainer title={'인기 Q&A'} />
+        <QnaContainer title={'최근 Q&A'} />
       </div>
     </div>
   );
