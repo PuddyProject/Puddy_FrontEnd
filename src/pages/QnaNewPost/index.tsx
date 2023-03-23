d428205cc4ddb96de3e1c2b3e2d8181a280233a8
vjvl95 <vjvl95@naver.com>
2023-03-24 02:30:13 +0900

    feat(QnaNewPost): useState 및 Handler 함수 추가

--- src/pages/QnaNewPost/index.tsx
+++ src/pages/QnaNewPost/index.tsx
@@ -1,38 +1,121 @@
 import InputBox from 'components/common/InputBox';
 import InputTilte from 'components/common/InputTitle';
 import FooterButton from 'components/common/FooterButton';
+import { useEffect, useState, FormEvent } from 'react';
+import { categoryItem } from 'constants/qnaNewPost';
+import axios from 'axios';
+interface PostInfo {
+  title: string;
+  content: string;
+  category: string;
+}
 export default function NewPost() {
+  const [postInfo, setPostInfo] = useState<PostInfo>({
+    content: '',
+    category: '',
+    title: '',
+  });
+  const [filePreview, setFilePreview] = useState<string[]>([]);
+  const [imgFile, setImgFile] = useState<File[]>([]);
+  const onChangeHandler = (e: FormEvent<HTMLElement>) => {
+    if ((e.target as HTMLInputElement).id === 'imgFile') {
+      return;
+    } else {
+      setPostInfo((prev) => ({
+        ...prev,
+        [(e.target as HTMLInputElement).id]: (e.target as HTMLInputElement).value,
+      }));
+    }
+  };
+
+  const onClickHandler = (e: React.MouseEvent) => {
+    setPostInfo((prev) => ({
+      ...prev,
+      [(e.target as HTMLButtonElement).id]: (e.target as HTMLButtonElement).innerText,
+    }));
+  };
+
+  const onFileChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (filePreview.length >= 3) {
+      alert('최대 파일 갯수는 3개 입니다.');
+      return;
+    }
+    const files = e.target.files as FileList;
+    // eslint-disable-next-line semi-spacing
+    for (let i = 0; i < files.length; i++) {
+      setImgFile((prev) => [...prev, files[i]]);
+      setFilePreview((prev) => [...prev, URL.createObjectURL(files[i])]);
+    }
+  };
+
+  const onSendDate = () => {
+    const formData = new FormData();
+    formData.append('requiest', new Blob([JSON.stringify(postInfo)], { type: 'application/json' }));
+    formData.append('imgFile', imgFile[0]);
+  };
+
   return (
     <div>
-      <div className='qna-newpost-main'>
+      <div className='qna-newpost-main' onChange={onChangeHandler}>
         <InputTilte isRequire={true}>카테고리 </InputTilte>
         <div className='category-container'>
-          {Array(6)
-            .fill(0)
-            .map(() => {
-              return <div className='category-item'>텍스트</div>;
-            })}
+          {categoryItem.map((category) => {
+            const isSlected = category === postInfo.category;
+
+            return (
+              <div
+                className={`category-item ${isSlected ? 'select' : ''}`}
+                id='category'
+                onClick={onClickHandler}
+              >
+                {category}
+              </div>
+            );
+          })}
         </div>
         <InputTilte isRequire={true}> 제목 </InputTilte>
         <InputBox
+          id='title'
           margin='10px 0px 20px 0px'
           padding='0px 0px 0px 15px'
           width='300px'
           placeholder='제목을 입력해주세요.(50자 이내)'
+          value={postInfo.title}
         />
         <div className='image-container'>
-          <div className='image-add-item'>
-            <p className='image-add-item-plus'>+</p>
-            <p className='image-add-item-text'>이미지 등록</p>
+          <input
+            id='imgFile'
+            className='image-add-item'
+            style={{ display: 'none' }}
+            type='file'
+            accept='image/*'
+            onChange={onFileChangeHandler}
+          />
+          <div className='image-item-add-button'>
+            <label htmlFor='imgFile'>
+              <p className='image-add-item-plus'>+</p>
+              <p className='image-add-item-text'>이미지 등록</p>
+            </label>
           </div>
-          <div className='image-item'></div>
-          <div className='image-item'></div>
-          <div className='image-item'></div>
+          {Array(3)
+            .fill(0)
+            .map((_, i) => {
+              return filePreview[i] !== undefined ? (
+                <img className='image-item' src={filePreview[i]} alt='error' />
+              ) : (
+                <div className='image-item' />
+              );
+            })}
         </div>
         <InputTilte isRequire={true}> 내용 </InputTilte>
-        <textarea className='text-body' placeholder='내용을 입력해주세요.(500자 이내)'></textarea>
+        <textarea
+          id='content'
+          className='text-body'
+          placeholder='내용을 입력해주세요.(500자 이내)'
+          defaultValue={postInfo.content}
+        ></textarea>
       </div>
-      <FooterButton>등록하기</FooterButton>
+      <FooterButton onClick={onSendDate}>등록하기</FooterButton>
     </div>
   );
 }
