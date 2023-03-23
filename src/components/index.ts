0706933f9d853ad6ee9eb20ca49f91b39c4349ae
vjvl95 <vjvl95@naver.com>
2023-03-24 02:28:10 +0900

    chore: MainQnAcard 및 Qna카드 export문 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 export { default as Button } from './common/Button';
 export { default as InputBox } from './common/InputBox';
 export { default as InputTitle } from './common/InputTitle';
-export { default as QnACard } from './common/MainQnaCard';
+export { default as MainQnACard } from './common/MainQnaCard';
 export { default as Message } from './common/Message';
 export { default as TextArea } from './common/TextArea';
 export { default as Checkbox } from './common/Checkbox';
 export { default as RadioButton } from './common/RadioButton';
+export { default as QnaCard } from './common/QnaCard';
