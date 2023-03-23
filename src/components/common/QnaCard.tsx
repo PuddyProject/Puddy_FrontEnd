64f1ca33c35906538a3a7401072cb2a3aa3ca554
vjvl95 <vjvl95@naver.com>
2023-03-24 02:28:35 +0900

    feat(QnaCard): Q&A 목록 페이지에 들어갈 Q&A 카드 완성

---
+++ src/components/common/QnaCard.tsx
@@ -0,0 +1,17 @@
+export default function QnaCard() {
+  return (
+    <div className='qna-card'>
+      <p className='title'>
+        <span className='title-category'>Q. [카테고리]</span> 강아지가 계속 떠들어서 너무 힘들어요
+      </p>
+      <div className='body'>
+        내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해 주세요.내용을 입력해 주세요.내용을
+        입력...
+      </div>
+      <div className='bottom-section'>
+        <span className='answer-text'>채택 완료</span>
+        <span className='writer-text'>작성자</span>
+      </div>
+    </div>
+  );
+}
