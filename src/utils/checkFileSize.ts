9adc4b6ba24e26c527d71c101f8ec9eb226db647
ReturnReason <reasonz@daum.net>
2023-03-24 19:19:26 +0900

    refactor(checkFileSize): 상수 변수 함수 바깥으로 이동

--- src/utils/checkFileSize.ts
+++ src/utils/checkFileSize.ts
@@ -1,6 +1,6 @@
-export default function checkFileSize(files: FileList) {
-  const MAX_FILE_SIZE = 10 * 1024 * 1024;
+const MAX_FILE_SIZE = 10 * 1024 * 1024;
 
+export default function checkFileSize(files: FileList) {
   for (const file of [...files]) {
     const size = file.size;
     if (size > MAX_FILE_SIZE) return false;
