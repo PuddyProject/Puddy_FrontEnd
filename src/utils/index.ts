e565edc2108804846e9bdcc98855a3cedac5d97b
ReturnReason <reasonz@daum.net>
2023-03-26 20:13:24 +0900

    feat: 회원가입 폼 validate 체크 함수 모듈 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,9 @@
+/** FILE */
 export { default as checkFileSize } from './checkFileSize';
 export { default as checkExtensions } from './checkExtensions';
+
+/** AXIOS */
 export { get, post } from './axiosHelper';
+
+/** VALIDATE */
+export { isValidId, isValidPw, isValidName } from './validate/checkSignup';
