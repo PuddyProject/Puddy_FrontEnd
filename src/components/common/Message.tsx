a72501cb2a936c7af445fa72106dbed02e9ca2af
ReturnReason <reasonz@daum.net>
2023-03-26 20:11:53 +0900

    feat(Message): children, alignRight 프롭스 추가

--- src/components/common/Message.tsx
+++ src/components/common/Message.tsx
@@ -1,8 +1,19 @@
+import React from 'react';
+
 interface MessageProps {
-  message?: string;
+  children?: React.ReactNode;
   isWarning?: boolean;
+  alignRight?: boolean;
 }
 
-export default function Message({ message, isWarning }: MessageProps) {
-  return <strong className={`${isWarning ? 'warning-message' : ''}`}>{message}</strong>;
+export default function Message({ isWarning, alignRight, children }: MessageProps) {
+  return (
+    <strong
+      className={`message ${isWarning ? 'warning-message' : 'correct-message'} ${
+        alignRight && 'align-right'
+      }`}
+    >
+      {children}
+    </strong>
+  );
 }
