8864c32115bee4c89972115a7cef4da828c0a113
ReturnReason <reasonz@daum.net>
2023-03-27 23:07:38 +0900

    feat: MyProfile 라우터 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,6 +18,7 @@ import {
   ExpertProfile,
   ExpertProfileEditor,
   MyActivityInfo,
+  MyProfile,
 } from 'pages';
 
 export default function Router() {
@@ -44,6 +45,7 @@ export default function Router() {
         <Route path='/profile/experts' element={<ExpertProfileEditor />} />
 
         {/* 프로필 보기 */}
+        <Route path='/mypage/profile' element={<MyProfile />} />
         <Route path='/experts/:id' element={<ExpertProfile />} />
         <Route path='/mypage/pets' element={<PetProfile />} />
       </Route>
