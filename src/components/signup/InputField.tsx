e0ca237c76f1e4a9d3bb8acf584bdeaf35abd17b
ReturnReason <reasonz@daum.net>
2023-03-30 04:23:11 +0900

    feat(InputField): InputField 컴포넌트 추가

---
+++ src/components/signup/InputField.tsx
@@ -0,0 +1,51 @@
+import { InputBox, InputTitle, Message } from 'components';
+import { FieldName, MemberShip } from 'pages/Signup';
+
+interface InputFieldProps {
+  className?: string;
+  type?: string;
+  placeholder: string;
+  target: FieldName;
+  title: string;
+  registrationMembership: MemberShip;
+  initWarningMessage: MemberShip;
+  warningMessage: MemberShip;
+  children?: React.ReactNode;
+  onChange: (target: FieldName) => (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function InputField({
+  className,
+  target,
+  title,
+  registrationMembership,
+  initWarningMessage,
+  warningMessage,
+  onChange,
+  children,
+  type = 'text',
+  placeholder = '',
+}: InputFieldProps) {
+  return (
+    <>
+      <div className='title-message'>
+        <InputTitle isRequire>{title}</InputTitle>
+        <Message isWarning alignRight>
+          {registrationMembership[target] === ''
+            ? initWarningMessage[target]
+            : warningMessage[target]}
+        </Message>
+      </div>
+      <div className={className ? className : ''}>
+        <InputBox
+          onChange={onChange(target)}
+          required
+          width='100%'
+          type={type}
+          placeholder={placeholder}
+        />
+        {children}
+      </div>
+    </>
+  );
+}
