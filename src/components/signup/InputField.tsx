ec19ae217c1ca8fc1433bc7f3bd7639a6abab1c6
ReturnReason <reasonz@daum.net>
2023-03-31 18:39:36 +0900

    feat(InputField): inputRef, showCorrectMessage 프롭스 추가

--- src/components/signup/InputField.tsx
+++ src/components/signup/InputField.tsx
@@ -2,6 +2,7 @@ import { InputBox, InputTitle, Message } from 'components';
 import { FieldName, Membership } from 'pages/Signup';
 
 interface InputFieldProps {
+  inputRef?: React.RefObject<HTMLInputElement>;
   className?: string;
   type?: string;
   placeholder: string;
@@ -10,34 +11,47 @@ interface InputFieldProps {
   registrationMembership: Membership;
   initWarningMessage: Membership;
   warningMessage: Membership;
+  showCorrectMessage?: { account: boolean; email: boolean };
   children?: React.ReactNode;
   onChange: (target: FieldName) => (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function InputField({
+  inputRef,
   className,
   target,
   title,
   registrationMembership,
   initWarningMessage,
   warningMessage,
+  showCorrectMessage,
   onChange,
   children,
   type = 'text',
   placeholder = '',
 }: InputFieldProps) {
+  const isAccountOrEmail = target === 'account' || target === 'email';
+
   return (
     <>
       <div className='title-message'>
         <InputTitle isRequire>{title}</InputTitle>
-        <Message isWarning alignRight>
-          {registrationMembership[target] === ''
-            ? initWarningMessage[target]
-            : warningMessage[target]}
-        </Message>
+        {isAccountOrEmail && showCorrectMessage?.[target] ? (
+          <Message>
+            사용 가능한 {(target === 'account' && '아이디') || (target === 'email' && '이메일')}
+            입니다.
+          </Message>
+        ) : (
+          <Message isWarning alignRight>
+            {registrationMembership[target] === ''
+              ? initWarningMessage[target]
+              : warningMessage[target]}
+          </Message>
+        )}
       </div>
       <div className={className ? className : ''}>
         <InputBox
+          inputRef={inputRef}
           onChange={onChange(target)}
           required
           width='100%'
