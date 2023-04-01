5578788a75ac9143f29fb20e0f657a7b0bf403da
vjvl95 <vjvl95@naver.com>
2023-04-02 01:27:42 +0900

    feat(QnaCard): 데이터 통신 후 데이터 바인딩 완료

--- src/components/common/QnaCard.tsx
+++ src/components/common/QnaCard.tsx
@@ -1,21 +1,20 @@
-export default function QnaCard() {
+import { QnaData } from 'types/qnaCardTypes';
+
+interface QnaCardProps {
+  qnaData: QnaData;
+}
+export default function QnaCard({ qnaData }: QnaCardProps) {
   return (
     <div className='qna-card'>
       <p className='title'>
-        <span className='title-category'>Q. [카테고리]</span> 강아지가 하루종일 하울링을 해요.
+        <span className='title-category'>Q. [{qnaData.category}]</span> {qnaData.title}
       </p>
-      <div className='body'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-        galley of type and scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-        passages, and more recently with desktop publishing software like Aldus PageMaker including
-        versions of Lorem Ipsum.
-      </div>
+      <div className='body'>{qnaData.content}</div>
       <div className='bottom-section'>
-        <span className='answer-text'>채택 완료</span>
-        <span className='writer-text'>작성자</span>
+        <span className={`answer-text ${qnaData.isSolved ? 'solved' : ''}`}>
+          {qnaData.isSolved === false ? '채택 대기' : '채택 완료'}
+        </span>
+        <span className='writer-text'>{qnaData.nickname}</span>
       </div>
     </div>
   );
