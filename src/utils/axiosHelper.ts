ec507829026c77426d71fc44dcc7cafd1e2fa85d
vjvl95 <vjvl95@naver.com>
2023-04-02 01:36:49 +0900

    refactor(axiosHelper): get 함수 공통으로 사용가능하게 리펙토링

--- src/utils/axiosHelper.ts
+++ src/utils/axiosHelper.ts
@@ -13,24 +13,20 @@ interface POST {
   body?: Object;
 }
 
-export async function get({ endpoint, params = '', queryString = '' }: GET) {
-  console.log(`%cGET 요청 ${SERVER_URL}${endpoint}/${params}${queryString}`, 'color: #a25cd1;');
+export async function get({ endpoint, params = '' }: GET) {
+  console.log(`%cGET 요청 ${SERVER_URL}${endpoint}${params}`, 'color: #a25cd1;');
 
-  return (
-    axios.get(`${SERVER_URL}${endpoint}/${params}`),
-    {
-      headers: {
-        Authorization: sessionStorage.getItem('userToken'),
-      },
-    }
-  );
+  return axios.get(`${SERVER_URL}${endpoint}${params}`, {
+    headers: {
+      Authorization: sessionStorage.getItem('userToken'),
+    },
+  });
 }
 
 export async function post({ endpoint, body }: POST) {
   const bodyData = JSON.stringify(body);
   console.log(`%cPOST 요청:${SERVER_URL}${endpoint}`, 'color: #296aba;');
   console.log(`%cPOST 요청 데이터: ${bodyData}`, 'color: #296aba;');
-
   return axios.post(`${SERVER_URL}${endpoint}`, bodyData, {
     headers: {
       'Content-Type': 'application/json',
