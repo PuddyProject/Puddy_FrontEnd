162af1e2e8560a7dd065c07bab625871848d28e8
vjvl95 <vjvl95@naver.com>
2023-04-02 04:01:56 +0900

    feat(Comment): 답글 데이터 바인딩

--- src/components/qnaDetail/Comment.tsx
+++ src/components/qnaDetail/Comment.tsx
@@ -1,23 +1,49 @@
 import Button from 'components/common/Button';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
+import { AnswerInfo } from 'types/commentTypes';
 import classnames from 'classnames';
+import { patch } from 'utils';
+import { useLocation } from 'react-router-dom';
 
 const cn = classnames;
 interface CommentProps {
+  answerData: AnswerInfo;
+  postWriterName?: string;
+  isSolved?: boolean;
   isExport?: boolean;
   isWriteUser?: boolean;
 }
 
-export default function Comment({ isExport = false, isWriteUser = false }: CommentProps) {
+export default function Comment({
+  answerData,
+  isSolved,
+  postWriterName,
+  isExport = false,
+  isWriteUser = false,
+}: CommentProps) {
+  const location = useLocation();
+
+  const selectAnswer = async () => {
+    const res = await patch({
+      endpoint: `questions/${location.pathname.split('/')[3]}/answers/${answerData.id}`,
+    });
+    if (res.status === 200) {
+      alert('답글이 채택 되었습니다.');
+      window.location.reload();
+    } else {
+      alert('오류가 생겼습니다. 잠시 후 다시 시도해주세요.');
+    }
+  };
+
   return (
     <div className={cn('comment-card', { isExport })}>
       <div className='comment-card-user-info'>
         {isExport ? (
           <div className='user-export'>
-            <span>이유</span> <AiOutlineCheckCircle />
+            <span>{answerData.nickname}</span> <AiOutlineCheckCircle />
           </div>
         ) : (
-          <div className='user'>유토</div>
+          <div className='user'>{answerData.nickname}</div>
         )}
 
         <div className='user-roll-container'>
@@ -29,22 +55,20 @@ export default function Comment({ isExport = false, isWriteUser = false }: Comme
           )}
         </div>
       </div>
-      <div className='comment-card-body'>
-        두달 반 정도 된 강아지 인데 사람이 앉으면 올라온 뒤 옷이나 팔찌등을 계속 물어요. 간식등으로
-        교육을 하려 할때도 흥분해서 간식든 손을 마구 물기만 합니다. 누워있을때 코를 툭치며 물려고
-        한적도 있어요. 무시하기, 소리내고 벌떡일어나기 아무것도 소용이 없어요. 바디블로킹을 하면
-        뒤로갔다가 달려들며 발을 물어요 ㅠㅠ 나중에는 제가 발로차는 느낌이 들어서 제가 애를
-        학대하는게 아닌가 걱정도 됩니다. 계속 압박하면 구석에숨다가 똥이나 오줌을 싸는데, 제가 애를
-        괴롭히는것 같아 걱정입니다... 어떻게 해야 할까요.....두달 반 정도 된 강아지 인데 사람이
-        앉으면 올라온 뒤 옷이나 팔찌등을 계속 물어요. 간식등으로 교육을 하려 할때도 흥분해서 간식든
-        손을 마구 물기만 합니다. 누워있을때 코를 툭치며 물려고 한적도 있어요. 무시하기, 소리내고
-        벌떡일어나기 아무것도 소용이 없어요. 바디블로킹을 하면 뒤로갔다가 달려들며 발을 물어요 ㅠㅠ
-        나중에는 제가 발로차는 느낌이 들어서 제가 애를 학대하는
-      </div>
+      <div className='comment-card-body'>{answerData.content}</div>
       <div className='comment-button'>
-        <Button width='110px' height='25px' fontWeight='600' fontSize='12px'>
-          이 답변 채택하기
-        </Button>
+        {!isSolved && (
+          <Button
+            width='110px'
+            height='25px'
+            fontWeight='600'
+            fontSize='12px'
+            margin='15px 0px 0px 0px'
+            onClick={selectAnswer}
+          >
+            이 답변 채택하기
+          </Button>
+        )}
       </div>
     </div>
   );
