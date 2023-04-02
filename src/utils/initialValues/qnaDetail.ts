7f5ce09597eb9fd07d2c38cebe784bf8329b2b51
vjvl95 <vjvl95@naver.com>
2023-04-02 16:25:33 +0900

    feat: QnaDetail에서 쓰일 init 값 추가

---
+++ src/utils/initialValues/qnaDetail.ts
@@ -0,0 +1,11 @@
+export const initQnaDetail = {
+  category: '',
+  content: '',
+  createdDate: '',
+  nickname: '',
+  title: '',
+  postCategory: 0,
+  questionId: 0,
+  viewCount: 0,
+  isSolved: false,
+};
