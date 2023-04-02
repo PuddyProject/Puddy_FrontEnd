61d277ee3a5d5973453ab38c1fa393c421f7023c
ReturnReason <reasonz@daum.net>
2023-04-02 15:27:49 +0900

    feat(TextArea): onChange 프롭스 추가

--- src/components/common/TextArea.tsx
+++ src/components/common/TextArea.tsx
@@ -1,8 +1,16 @@
 interface TextAreaProps {
   placeholder?: string;
   readonly?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-export default function TextArea({ placeholder, readonly }: TextAreaProps) {
-  return <textarea readOnly={readonly} className='text-area' placeholder={placeholder} />;
+export default function TextArea({ placeholder, readonly, onChange }: TextAreaProps) {
+  return (
+    <textarea
+      onChange={onChange}
+      readOnly={readonly}
+      className='text-area'
+      placeholder={placeholder}
+    />
+  );
 }
