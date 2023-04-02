1523171b76c32b3a080849a3f45896c00798ba53
vjvl95 <vjvl95@naver.com>
2023-04-03 00:43:27 +0900

    feat(MainQnaCard): 메인페이지 Q&A 카드 데이터 바인딩 작업

--- src/components/main/MainQnaCard.tsx
+++ src/components/main/MainQnaCard.tsx
@@ -1,11 +1,18 @@
-export default function MainQnaCard() {
+import { MainQnaCardType } from 'types/qnaCardTypes';
+
+interface MainQnaCardProps {
+  cardData: MainQnaCardType;
+}
+export default function MainQnaCard({ cardData }: MainQnaCardProps) {
   return (
     <div className='main-qna-card'>
-      <p className='title'>강아지가 분리불안을 겪어서 너무 힘들어요</p>
-      <p className='body'>아침에 오는데 너무 힘들어서 분리불안을 겪는 중이에요</p>
+      <p className='title'>{cardData.title}</p>
+      <p className='body'>{cardData.content}</p>
       <div className='bottom-section'>
-        <span className='answer-text'>채택 완료</span>
-        <span className='writer-text'>작성자</span>
+        <span className={`answer-text ${cardData.isSolved ? 'solved' : ''}`}>
+          {cardData.isSolved ? '채택 완료' : '채택 대기'}
+        </span>
+        <span className='writer-text'>{cardData.nickname}</span>
       </div>
     </div>
   );
