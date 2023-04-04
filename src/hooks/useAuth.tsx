f41c1abeb4e878944ead5d3a6696c88d528a01c5
ReturnReason <reasonz@daum.net>
2023-04-05 02:12:35 +0900

    feat(useAuth): 토큰 디코드 관련 useEffect 추가

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,22 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { UserToken } from 'types/userTokenTypes';
 
 import decodeJWT from 'utils/decodeJWT';
 
 export function useAuth() {
-  const [userToken, setUserToken] = useState<string | null>(sessionStorage.getItem('userToken'));
+  const [userToken, setUserToken] = useState<string | null>(null);
+  const [userDecodedToken, setUserDecodedToken] = useState<UserToken | null>(null);
 
+  useEffect(() => {
+    const token = sessionStorage.getItem('userToken');
+    setUserToken(() => token);
+    if (userToken) {
+      decodedUserToken();
+    }
+  }, [userToken]);
+
+  // * 토큰 디코드
   const decodedUserToken = () => {
     if (!userToken) return null;
 
     const decodedToken = decodeJWT(userToken);
-    return decodedToken;
+    setUserDecodedToken(() => decodedToken);
   };
 
+  // * 토큰 설정
   const setToken = (token: string) => {
     sessionStorage.setItem('userToken', token);
     setUserToken(token);
   };
 
+  // * 토큰 삭제
   const removeToken = () => {
     sessionStorage.removeItem('userToken');
     setUserToken(null);
@@ -30,5 +43,5 @@ export function useAuth() {
     return decodedToken && decodedToken.exp > Date.now() / 1_000;
   };
 
-  return { decodedUserToken, userToken, setToken, removeToken, isValidToken };
+  return { userDecodedToken, userToken, setToken, removeToken, isValidToken };
 }
