2fdeaf8b8756bc6d47b151e545512d164d298db0
ReturnReason <reasonz@daum.net>
2023-04-06 23:48:47 +0900

    feat(App): petProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from 'react-router-dom';
 import Router from './routes';
 
 import { UserProvider } from 'context/UserContext';
+import { PetProvider } from 'context/PetContext';
 
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <PetProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </PetProvider>
     </UserProvider>
   );
 }
