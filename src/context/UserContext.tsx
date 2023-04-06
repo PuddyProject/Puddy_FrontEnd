fc768569f4f346671106f161cf64b272d8fa4365
ReturnReason <reasonz@daum.net>
2023-04-06 09:02:03 +0900

    feat(UserContext): 전역 상태 관리를 위한 UserContext 추가

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,34 +1,70 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
-import { useAuth } from 'hooks/useAuth';
-
 import { UserToken } from 'types/userTokenTypes';
+import decodeJWT from 'utils/decodeJWT';
 
 interface UserContextProps {
-  loggedInUser: UserToken | null;
-  setLoggedInUser: React.Dispatch<React.SetStateAction<UserToken | null>>;
+  token: string | null;
+  decodedToken: UserToken | null;
+  setToken: React.Dispatch<React.SetStateAction<string | null>>;
+  setDecodedToken: React.Dispatch<React.SetStateAction<UserToken | null>>;
 }
 
-const UserContext = createContext<UserContextProps>({
-  loggedInUser: null,
-  setLoggedInUser: () => {},
+export const UserContext = createContext<UserContextProps>({
+  token: null,
+  decodedToken: null,
+  setToken: () => {},
+  setDecodedToken: () => {},
 });
 
+const TOKEN_KEY = 'userToken';
+
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
-  const [loggedInUser, setLoggedInUser] = useState<UserToken | null>(null);
-  const { userDecodedToken } = useAuth();
+  const [token, setToken] = useState<string | null>(null);
+  const [decodedToken, setDecodedToken] = useState<UserToken | null>(null);
+
+  const contextValue = {
+    token,
+    decodedToken,
+    setToken,
+    setDecodedToken,
+  };
 
   useEffect(() => {
-    setLoggedInUser(() => userDecodedToken);
-  }, [userDecodedToken]);
+    const storedToken = sessionStorage.getItem(TOKEN_KEY);
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, [token]);
 
-  return (
-    <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
-      {children}
-    </UserContext.Provider>
-  );
+  useEffect(() => {
+    if (token) {
+      const decoded = decodeJWT(token);
+      setDecodedToken(() => decoded);
+    } else setDecodedToken(null);
+  }, [token]);
+
+  useEffect(() => {
+    const watchedStorageToken = (e: StorageEvent) => {
+      if (e.key !== TOKEN_KEY) return;
+
+      const userToken = e.newValue;
+      if (!userToken) {
+        setToken(() => null);
+        setDecodedToken(() => null);
+      }
+    };
+
+    window.addEventListener('storage', watchedStorageToken);
+
+    return () => {
+      window.removeEventListener('storage', watchedStorageToken);
+    };
+  }, []);
+
+  return <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>;
 };
 
-export const useUser = (): UserContextProps => {
-  return useContext<UserContextProps>(UserContext);
+export const useUser = () => {
+  return useContext(UserContext);
 };
