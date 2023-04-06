db0e7173f0e36b9ebdd4cc113f7be4abdf2eab4d
ReturnReason <reasonz@daum.net>
2023-04-06 09:02:58 +0900

    refator(useAuth): 디코드 관련 함수 제거, 함수 이름을 더 명확하게 변경

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,47 +1,29 @@
-import { useEffect, useState } from 'react';
-import { UserToken } from 'types/userTokenTypes';
+import { useState } from 'react';
 
 import decodeJWT from 'utils/decodeJWT';
 
 export function useAuth() {
   const [userToken, setUserToken] = useState<string | null>(null);
-  const [userDecodedToken, setUserDecodedToken] = useState<UserToken | null>(null);
-
-  useEffect(() => {
-    const token = sessionStorage.getItem('userToken');
-    setUserToken(() => token);
-    if (userToken) {
-      decodedUserToken();
-    }
-  }, [userToken]);
-
-  // * 토큰 디코드
-  const decodedUserToken = () => {
-    if (!userToken) return null;
-
-    const decodedToken = decodeJWT(userToken);
-    setUserDecodedToken(() => decodedToken);
-  };
 
   // * 토큰 설정
-  const setToken = (token: string) => {
+  const initSessionStorageUserToken = (token: string) => {
     sessionStorage.setItem('userToken', token);
     setUserToken(token);
   };
 
   // * 토큰 삭제
-  const removeToken = () => {
+  const removeSessionStorageUserToken = () => {
     sessionStorage.removeItem('userToken');
     setUserToken(null);
   };
 
   // * 토큰 만료 확인
-  const isValidToken = () => {
+  const isTokenExpired = () => {
     if (!userToken) return false;
 
     const decodedToken = decodeJWT(userToken);
     return decodedToken && decodedToken.exp > Date.now() / 1_000;
   };
 
-  return { userDecodedToken, userToken, setToken, removeToken, isValidToken };
+  return { userToken, initSessionStorageUserToken, removeSessionStorageUserToken, isTokenExpired };
 }
