fcafd1cd98cddca3b3da58907c1f7802d1cd3b25
ReturnReason <reasonz@daum.net>
2023-04-06 23:56:11 +0900

    feat(checkNickname): 닉네임 체크 정규식 변경

--- src/utils/validate/checkNickname.ts
+++ src/utils/validate/checkNickname.ts
@@ -1,4 +1,4 @@
-const NICKNAME_REG_EXP = /^[가-힣a-zA-Z0-9]{2,10}$/;
+const NICKNAME_REG_EXP = /^(?!퍼디)[가-힣a-zA-Z0-9]{2,10}$/;
 
 export function isValidNickname(nickname: string) {
   return NICKNAME_REG_EXP.test(nickname);
