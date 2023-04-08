bf60a73868be572f24fe91a5d537ac84ba092e6f
ReturnReason <reasonz@daum.net>
2023-04-09 01:31:58 +0900

    feat(navItem): 하단 앱바 아이콘 타입, 아이콘 컴포넌트 추가

--- src/constants/navItem.ts
+++ src/constants/navItem.ts
@@ -1,4 +1,10 @@
+import { AiOutlineHome as HomeIcon, AiOutlineComment as CommunityIcon } from 'react-icons/ai';
+import { BsPersonCircle as PersonIcon } from 'react-icons/bs';
+import { BiPencil as PencilIcon } from 'react-icons/bi';
+import { IconType } from 'react-icons';
+
 interface NavItem {
+  icon: IconType;
   path: string;
   title: string;
   memberOnly: boolean;
@@ -6,21 +12,25 @@ interface NavItem {
 
 export const navItems: NavItem[] = [
   {
+    icon: HomeIcon,
     path: '/',
     title: '홈',
     memberOnly: false,
   },
   {
+    icon: PencilIcon,
     path: '/qna',
     title: 'Q&A',
     memberOnly: false,
   },
   {
+    icon: CommunityIcon,
     path: '/community',
     title: '커뮤니티',
     memberOnly: false,
   },
   {
+    icon: PersonIcon,
     path: '/mypage',
     title: '마이페이지',
     memberOnly: true,
