bbc11a5dcd92071e4493fb47413e22e99a9965c0
REASON <reasonz@daum.net>
2023-04-13 04:58:49 +0900

    feat(routes): 라우터 path 상수 정의

---
+++ src/constants/routes.ts
@@ -0,0 +1,78 @@
+/* 메인 페이지 */
+export const HOME_PATH = '/';
+
+/* =========================================================== */
+
+/* Q&A 페이지 */
+export const QNA_PATH = '/qna';
+
+/* Q&A 게시글 상세보기 */
+export const QNA_DETAIL_PATH = '/qna/detail/:id';
+
+/* Q&A 게시글 답변 작성 */
+export const getPathWriteAnswer = (id: string = ':id') => {
+  return `/qna/detail/${id}/write/answer`;
+};
+
+/* Q&A 새 게시글 발행  */
+export const QNA_WRITE_POST_PATH = '/qna/newpost';
+
+/* Q&A 답변 작성 */
+// export const QNA_WRITE_ANSWER_PATH = 'write/answer';
+
+/* =========================================================== */
+
+/* 커뮤니티 페이지 */
+export const COMMUNITY_PATH = '/community';
+
+/* =========================================================== */
+
+/* 로그인 페이지 */
+export const LOGIN_PATH = '/auth/login';
+
+/* 소셜 로그인 - 카카오 */
+export const KAKAO_AUTH_PATH = '/oauth/authorize';
+
+/* 회원가입 페이지 */
+export const JOIN_PATH = '/auth/Signup';
+
+/* =========================================================== */
+
+/* 마이페이지 */
+export const MY_PAGE_PATH = '/mypage';
+
+/* 마이페이지 내가 쓴 게시글/댓글 */
+export const MY_POSTS_PATH = 'mypage/posts';
+
+/* 마이페이지 내 프로필 수정 */
+export const MY_PAGE_PROFILE_PATH = '/mypage/profile';
+
+/* 마이페이지 내 펫 프로필 보기 */
+export const MY_PAGE_PET_PATH = '/mypage/pets';
+
+/* 마이페이지 펫 프로필 등록하기 에디터*/
+export const PROFILE_PET_PATH = '/profile/pets';
+
+/* 마이페이지 펫 프로필 수정하기 에디터*/
+export const getPathPetProfile = (userId: string = ':id') => {
+  return `/profile/pets/${userId}`;
+};
+
+/* 마이페이지 전문가 등록 (서류제출) */
+export const MY_PAGE_AUTH_EXPERT_PATH = '/mypage/experts';
+
+/* 마이페이지 전문가 프로필 작성 */
+export const EXPERTS_PROFILE_PATH = '/profile/experts';
+
+/* 마이페이지 설정 - 계정 */
+export const MY_PAGE_ACCOUNT_PATH = '/mypage/account';
+
+/* =========================================================== */
+
+/* 펫 정보 보기 */
+export const PET_PROFILE_PATH = '/users/pets/detail';
+
+/* =========================================================== */
+
+/* 전문가 프로필 보기 */
+export const EXPERT_PROFILE_PATH = '/experts/:id';
