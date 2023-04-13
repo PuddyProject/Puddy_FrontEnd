af56b9776c376c950c12c286adce3278ce847649
vjvl95 <vjvl95@naver.com>
2023-04-14 02:16:56 +0900

    feat: CardList 페이지 에서 쓰일 상수 추가

--- src/constants/cardList.ts
+++ src/constants/cardList.ts
@@ -1,2 +1,27 @@
 export const FILTER_ITEM = ['최신순', '오래된순', '인기순'];
 export const PAGE_LIST = ['community', 'qna', 'expert'];
+
+export const TITLE = {
+  community: '커뮤니티 🐶',
+  qna: 'Q&A 🐶',
+};
+
+export const END_POINT = {
+  community: 'articles',
+  qna: 'questions',
+};
+
+export const LIST_NAME = {
+  community: 'articleList',
+  qna: 'questionList',
+};
+
+export const CARD_ID = {
+  community: 'articleId',
+  qna: 'questionId',
+};
+
+export const NO_POST = {
+  community: '커뮤니티 ',
+  qna: 'Q&A ',
+};
