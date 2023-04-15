5d25aa5b87fab1edf01558c88ea697331f15629c
vjvl95 <vjvl95@naver.com>
2023-04-15 22:04:50 +0900

    feat:ANSWER_LIST 상수 추가

--- src/constants/cardDetail.ts
+++ src/constants/cardDetail.ts
@@ -1,1 +1,5 @@
 export const PET_INFO = ['name', 'breed', 'age', 'gender', 'note', 'neutered'];
+export const ANSWER_LIST = {
+  community: 'commentList',
+  qna: 'answerList',
+};
--- src/constants/qnaNewPost.ts
+++
@@ -1,1 +0,0 @@
-export const categoryItem = ['훈련', '산책', '건강', '미용', '먹이', '기타'];
