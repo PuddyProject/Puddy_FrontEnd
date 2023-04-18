d970c5634ee5af1a6f68614239f159e010901467
vjvl95 <vjvl95@naver.com>
2023-04-18 21:55:15 +0900

    feat(_qna-main-card): 메인페이지 Q&A 카드 닉네임 이상하게 나오는 현상 수정

--- src/components/main/MainQnaCard.tsx
+++ src/components/main/MainQnaCard.tsx
@@ -9,10 +9,10 @@ export default function MainQnaCard({ cardData }: MainQnaCardProps) {
       <p className='title'>{cardData.title}</p>
       <p className='body'>{cardData.content}</p>
       <div className='bottom-section'>
-        <span className={`answer-text ${cardData.isSolved ? 'solved' : ''}`}>
+        <div className={`answer-text ${cardData.isSolved ? 'solved' : ''}`}>
           {cardData.isSolved ? '채택 완료' : '채택 대기'}
-        </span>
-        <span className='writer-text'>{cardData.nickname}</span>
+        </div>
+        <div className='writer-text'>{cardData.nickname}</div>
       </div>
     </div>
   );
