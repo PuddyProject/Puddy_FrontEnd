c0182931804cc749ad06905b84997eae8fbb4459
vjvl95 <vjvl95@naver.com>
2023-04-24 23:41:02 +0900

    fix: 페이지에 맞는 글자 나오도록 수정

--- src/pages/CommentAnswer/index.tsx
+++ src/pages/CommentAnswer/index.tsx
@@ -19,6 +19,8 @@ export default function CommentAnswer() {
   const isCommunityPage = location.pathname.includes('community');
   const isReviewPage = location.pathname.includes('reviews');
 
+  const currentMessage = isReviewPage ? '리뷰' : isCommunityPage ? '댓글' : '답변';
+
   useEffect(() => {
     textAreaRef.current?.focus({});
   }, []);
@@ -59,10 +61,10 @@ export default function CommentAnswer() {
 
     const editText = isEditPage ? '수정' : '작성';
     if (res.data.resultCode === 'SUCCESS') {
-      alert(`답변 ${editText} 완료`);
+      alert(`${currentMessage} ${editText} 완료`);
       nav(-1);
     } else {
-      alert(`답변 ${editText} 실패. 잠시 후 다시 시도해주세요`);
+      alert(`${currentMessage} ${editText} 실패. 잠시 후 다시 시도해주세요`);
     }
   }
 
@@ -74,18 +76,20 @@ export default function CommentAnswer() {
   return (
     <>
       <CustomHeader
-        title={`${isReviewPage ? '리뷰' : isCommunityPage ? '커뮤니티 댓글' : 'Q&A 답변'}  작성`}
+        title={`${isReviewPage ? '리뷰' : isCommunityPage ? '커뮤니티 댓글' : 'Q&A 답변'}  ${
+          isEditPage ? '수정' : '작성'
+        }`}
         hideIcon
       />
       <div>
         <InputTitle isRequire={true} margin='50px 0px 10px 0px'>
-          답변 내용
+          {currentMessage} 내용
         </InputTitle>
         <textarea
           className='text-body'
           maxLength={100}
           defaultValue={answer}
-          placeholder='답변을 입력해주세요.(최대 100자)'
+          placeholder={`${currentMessage} 을 입력해주세요.(최대 100자)`}
           ref={textAreaRef}
           onFocus={onFocus}
           onChange={(e) => {
@@ -94,7 +98,8 @@ export default function CommentAnswer() {
         />
       </div>
       <FooterButton onClick={sendData}>
-        {isReviewPage ? '리뷰 작성하기' : isEditPage ? '답변 수정하기' : '답변 작성하기'}
+        {isReviewPage ? '리뷰' : isCommunityPage ? '댓글' : '답변'}
+        {isEditPage ? ' 수정하기' : ' 작성하기'}
       </FooterButton>
     </>
   );
