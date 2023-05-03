9e8501f2d4e44d730716bdc4cc101b30bfbe1a2e
REASON <reasonz@daum.net>
2023-05-04 03:50:17 +0900

    chore(Kakao): 카카오 로그인 url 수정

--- src/constants/kakaoLogin.ts
+++ src/constants/kakaoLogin.ts
@@ -1,3 +1,3 @@
-export const REDIRECT_URI = 'http://localhost:3000/oauth/authorize';
+export const REDIRECT_URI = 'https://www.puddy.world/oauth/authorize';
 export const KAKAO_LOGIN_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 export const KAKAO_AUTH_URI = 'https://kauth.kakao.com/oauth/token';
