a9daea55b42874df4408ed123ed8f35512dc9193
REASON <reasonz@daum.net>
2023-05-04 02:22:56 +0900

    refactor(QnaCard): className을 조금 더 직관적으로 수정

--- src/components/common/QnaCard.tsx
+++ src/components/common/QnaCard.tsx
@@ -7,10 +7,10 @@ interface QnaCardProps {
 export default function QnaCard({ questionData }: QnaCardProps) {
   return (
     <div className='qna-card'>
-      <p className='title'>
+      <p className='card-title'>
         <span className='title-category'>Q. [{questionData.category}]</span> {questionData.title}
       </p>
-      <div className='body'>{questionData.content}</div>
+      <div className='card-content'>{questionData.content}</div>
       <div className='bottom-section'>
         <span className={`answer-text ${!questionData.isSolved ? '' : 'solved'}`}>
           {questionData.isSolved === false ? '채택 대기' : '채택 완료'}
