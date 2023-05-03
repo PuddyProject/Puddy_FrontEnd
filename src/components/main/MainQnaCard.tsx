8bde1c9184e7b85bc083b0779d12065cc39ba8dd
REASON <reasonz@daum.net>
2023-05-04 02:25:36 +0900

    feat(MainQnaCard): Q&A 카드 상단에 작성일 추가

--- src/components/main/MainQnaCard.tsx
+++ src/components/main/MainQnaCard.tsx
@@ -4,10 +4,16 @@ interface MainQnaCardProps {
   cardData: MainQnaCardType;
 }
 export default function MainQnaCard({ cardData }: MainQnaCardProps) {
+  const createDate = new Date(cardData.createdDate);
+  const [year, month, day] = createDate.toLocaleDateString().split('.');
+
   return (
     <div className='main-qna-card'>
-      <p className='title'>{cardData.title}</p>
-      <p className='body'>{cardData.content}</p>
+      <p className='card-date'>{`${year}.${month}.${day}`}</p>
+
+      <p className='card-title'>{cardData.title}</p>
+      <p className='card-content'>{cardData.content}</p>
+
       <div className='bottom-section'>
         <div className={`answer-text ${cardData.isSolved ? 'solved' : ''}`}>
           {cardData.isSolved ? '채택 완료' : '채택 대기'}
