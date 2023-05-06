50449dccf6e5dfd11a1df7293a6ca55609a4b911
REASON <reasonz@daum.net>
2023-05-06 20:08:28 +0900

    refactor(axiosTypes): 중복되는 interface 속성은 상속받아 사용하도록 변경

--- src/utils/axios/axiosTypes.ts
+++ src/utils/axios/axiosTypes.ts
@@ -1,30 +1,25 @@
-export interface GET {
+interface BaseHttpMethod {
   endpoint: string;
   params?: string;
   queryString?: string;
 }
 
-export interface DELETE {
-  endpoint: string;
-  params?: string;
-  queryString?: string;
-}
+export interface GET extends BaseHttpMethod {}
 
-export interface POST {
-  endpoint: string;
+export interface DELETE extends BaseHttpMethod {}
+
+export interface POST extends BaseHttpMethod {
   body?: object;
   isImage?: boolean;
   isPost?: boolean;
 }
 
-export interface PATCH {
-  endpoint: string;
+export interface PATCH extends BaseHttpMethod {
   body?: object | File;
   isFormData: boolean;
 }
 
-export interface PUT {
-  endpoint: string;
+export interface PUT extends BaseHttpMethod {
   body?: object | File;
   isFormData: boolean;
 }
