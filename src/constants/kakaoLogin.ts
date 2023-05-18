54416fae8aed5d62a0b1f060c7a5dc27bce940ac
REASON <reasonz@daum.net>
2023-05-19 02:25:39 +0900

    chore(kakaoLogin): 로컬 호스트용 URI 주소 주석 추가

--- src/constants/kakaoLogin.ts
+++ src/constants/kakaoLogin.ts
@@ -1,3 +1,4 @@
 export const REDIRECT_URI = 'https://www.puddy.world/oauth/authorize';
+// 로컬 호스트에서 테스트 시 사용:  'http://localhost:3000/oauth/authorize';
 export const KAKAO_LOGIN_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 export const KAKAO_AUTH_URI = 'https://kauth.kakao.com/oauth/token';
